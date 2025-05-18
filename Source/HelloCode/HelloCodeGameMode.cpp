a240ee761c94aa0b47917631ef6dc1f61e0c64a7
hdjin <hdjin@iglim.net>
2025-05-19 06:17:45 +0900

    INHANCE: FString연습

--- Source/HelloCode/HelloCodeGameMode.cpp
+++ Source/HelloCode/HelloCodeGameMode.cpp
@@ -26,5 +26,24 @@ void AHelloCodeGameMode::ScoreUp(unsigned int ScoreUp)
 void AHelloCodeGameMode::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("GameMode BeginPlay!"));
+
+	// 1 정석
+	const FString ClassName1 = FString::Printf(TEXT("%s BeginPlay, until %d!"), *FString("HelloCodeGameMode"), 2040);
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *ClassName1);
+
+	// 2 UE_LOG매크로 지원
+	const FString ClassName2 = TEXT("HelloCodeGameMode");
+	UE_LOG(LogTemp, Warning, TEXT("%s BeginPlay, until %d!"), *ClassName2, 2040);
+
+	// 3 Format() 가변인자 지원
+	const FString ClassName3 = TEXT("HelloCodeGameMode");
+	const FString OtherStringPart = TEXT("BeginPlay, until");
+	int32 Year = 2040;
+	TArray<FStringFormatArg> Args;
+	Args.Add(FStringFormatArg(ClassName3));
+	Args.Add(FStringFormatArg(OtherStringPart));
+	Args.Add(FStringFormatArg(Year));
+	const FString FormattedString = FString::Format(TEXT("{0} {1} {2}!"), Args); // 타입추론
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *FormattedString);
+	
 }

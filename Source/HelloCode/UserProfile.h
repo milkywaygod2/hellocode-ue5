53532834ba5d766dd455d6aecb672f5fd99c694b
hdjin <hdjin@iglim.net>
2025-05-26 06:33:28 +0900

    FEAT: 유저프로필시스템 추가

---
+++ Source/HelloCode/UserProfile.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "UserProfile.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class HELLOCODE_API UUserProfile : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	FString Name;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float HealthMax;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float Armor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Unit")
+	TSubclassOf<UObject> UClassOfPlayer;
+	
+	UPROPERTY(EditAnywhere, meta=(MetaClass="GameMode"), Category = "Unit")
+	FSoftClassPath UClassGameMode;
+};

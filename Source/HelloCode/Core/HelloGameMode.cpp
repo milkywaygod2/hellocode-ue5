25e178f542af633219f6ee3f7db23c179025f577
hdjin <hdjin@iglim.net>
2025-05-30 22:14:55 +0900

    FIX: 로그내용수정

--- Source/HelloCode/Core/HelloGameMode.cpp
+++ Source/HelloCode/Core/HelloGameMode.cpp
@@ -16,15 +16,15 @@ void AHelloGameMode::BeginPlay()
 	Super::BeginPlay();
 
 	// 1 정석
-	const FString ClassName1 = FString::Printf(TEXT("%s BeginPlay, until %d!"), *FString("HelloGameMode"), 2040);
+	const FString ClassName1 = FString::Printf(TEXT("%s BeginPlay, until %d!"), *FString("HelloGameModeEcho1"), 2040);
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *ClassName1);
 
 	// 2 UE_LOG매크로 지원
-	const FString ClassName2 = TEXT("HelloGameMode");
+	const FString ClassName2 = TEXT("HelloGameModeEcho2");
 	UE_LOG(LogTemp, Warning, TEXT("%s BeginPlay, until %d!"), *ClassName2, 2040);
 
 	// 3 Format() 가변인자 지원
-	const FString ClassName3 = TEXT("HelloGameMode");
+	const FString ClassName3 = TEXT("HelloGameModeEcho3");
 	const FString OtherStringPart = TEXT("BeginPlay, until");
 	int32 Year = 2040;
 	TArray<FStringFormatArg> Args;

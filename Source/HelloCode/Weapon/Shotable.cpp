a7d3b98655483421da4c43b3ffec1c2d0b3cc679
hdjin <hdjin@iglim.net>
2025-06-06 13:28:15 +0900

    #9 주요 기능 선언 및 윤곽작업

--- Source/HelloCode/Weapon/Shotable.cpp
+++ Source/HelloCode/Weapon/Shotable.cpp
@@ -2,3 +2,20 @@
 
 
 #include "Shotable.h"
+
+UShotable::UShotable()
+{
+}
+
+void UShotable::Shot()
+{
+}
+
+void UShotable::AttachWeapon(AHelloCharacter* TargetCharacter)
+{
+}
+
+void UShotable::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
--- Source/HelloCode/Weapon/Shotable.h
+++ Source/HelloCode/Weapon/Shotable.h
@@ -9,8 +9,56 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class HELLOCODE_API UShotable : public USkeletalMeshComponent
 {
 	GENERATED_BODY()
+public:
+	/** Can be nullptr, some Shotable-thing could be mounted out of sight like a missle in the cell */	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Shot", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* MeshCollision;
+	
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category=Projectile)
+	TSubclassOf<class AHelloProjectile> ShotProjectile;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+	USoundBase* ShotSound;
+	
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	UAnimMontage* ShotAnimation;
+
+	/** Gun muzzle's offset from the characters location */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+	FVector MuzzleOffset;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	class UInputMappingContext* ShotMappingContext;
+
+	/** Shot Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	class UInputAction* ShotAction;
+
+	/** Sets default values for this component's properties */
+	UShotable();
+
+	/** Make the weapon Shot a Projectile */
+	UFUNCTION(BlueprintCallable, Category="Weapon")
+	void Shot();
+
+	/** Attaches the actor to a FirstPersonCharacter */
+	UFUNCTION(BlueprintCallable, Category="Weapon")
+	void AttachWeapon(AHelloCharacter* TargetCharacter);
+
+protected:
+	/** Ends gameplay for this component. */
+	UFUNCTION()
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	/** The Character holding this weapon*/
+	AHelloCharacter* Character;
 };

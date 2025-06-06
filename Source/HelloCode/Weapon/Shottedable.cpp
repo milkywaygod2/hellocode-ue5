9795045a1aaa4573e02270b5a58123af5a7e33d1
hdjin <hdjin@iglim.net>
2025-06-06 23:49:32 +0900

    #9 COMMENT: 코드정리

--- Source/HelloCode/Weapon/Shottedable.cpp
+++ Source/HelloCode/Weapon/Shottedable.cpp
@@ -29,17 +29,17 @@ void UShottedable::Shot()
 		FActorSpawnParameters ActorSpawnParams;
 		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
 
-		// Spawn the projectile at the muzzle
+		// 1.액터생성
 		World->SpawnActor<AHelloProjectile>(ShotProjectile, SpawnLocation, SpawnRotation, ActorSpawnParams);
 	}
 	
-	// Try and play the sound if specified
+	// 2.사운드 재생
 	if (ShotSound)
 	{
 		UGameplayStatics::PlaySoundAtLocation(this, ShotSound, Character->GetActorLocation());
 	}
 	
-	// Get the animation object for the arms mesh
+	// 3.애니메이션 재생
 	if (UAnimInstance* AnimInstance = Character->GetMesh1P()->GetAnimInstance(); AnimInstance && ShotAnimation)
 	{
 		AnimInstance->Montage_Play(ShotAnimation, 1.f);
@@ -56,18 +56,18 @@ void UShottedable::AttachWeapon(AHelloCharacter* TargetCharacter)
 	AttachToComponent(Character->GetMesh1P(), AttachmentRules, FName(TEXT("GripPoint")));
 	Character->SetEquipShottedable(true);
 
-	// Set up action bindings
 	if (const APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
 	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		// 1. 인풋 키매핑 컨텍스트 추가
+		if (UEnhancedInputLocalPlayerSubsystem* PlayerEnhancedInputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
 		{
-			// Set the priority of the mapping to 1, so that it overrides the Jump action with the ShotAction when using touch input
-			Subsystem->AddMappingContext(ShotMappingContext, 1);
+			PlayerEnhancedInputSubsystem->AddMappingContext(ShotMappingContext, 1); // 키 매핑의 누적적 추가, 매핑중복시 0 보다 더 높은 우선순위
 		}
 
-		if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+		// 2. 인풋 콜백함수 바인딩
+		if (UEnhancedInputComponent* PlayerEnhancedInput = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
 		{
-			EnhancedInputComponent->BindAction(ShotAction, ETriggerEvent::Triggered, this, &UShottedable::Shot);
+			PlayerEnhancedInput->BindAction(ShotAction, ETriggerEvent::Triggered, this, &UShottedable::Shot);
 		}
 	}
 }
@@ -79,9 +79,10 @@ void UShottedable::EndPlay(const EEndPlayReason::Type EndPlayReason)
 
 	if (const APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
 	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		// 3. 인풋 키매핑 컨텍스트 제거
+		if (UEnhancedInputLocalPlayerSubsystem* PlayerEnhancedInputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
 		{
-			Subsystem->RemoveMappingContext(ShotMappingContext);
+			PlayerEnhancedInputSubsystem->RemoveMappingContext(ShotMappingContext);
 		}
 	}
 }

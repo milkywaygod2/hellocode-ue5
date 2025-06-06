4dbb75bc2e9c1ea2ef93dac574fd98d04e906e3c
hdjin <hdjin@iglim.net>
2025-06-06 13:21:56 +0900

    #9 FEAT: 쏘는 기능, 스켈레톤매시는 상속대신 소유컨셉으로 (눈에 안보이는것도 있을수있으니? - 함정미사일셀의 여러가지미사일등..)

---
+++ Source/HelloCode/Weapon/Shotable.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Shotable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOCODE_API UShotable : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+};
